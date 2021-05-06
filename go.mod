ad0f679421701f055c900ff17eba3d5e4c458dc3
Maykon Mori <maykonmori@gmail.com>
2021-05-06 12:27:09 -0300

    ATT

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/maykonmori/kafka_estudos
+
+go 1.16
+
+require (
+	github.com/Shopify/sarama v1.28.0 // indirect
+	github.com/klauspost/compress v1.12.2 // indirect
+	golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
+	golang.org/x/net v0.0.0-20210505214959-0714010a04ed // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Shopify/sarama"
+)
+
+const (
+	kafkaConn = "localhost:8000"
+	topic     = "kafka-headers"
+)
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("### Kafka Producer ###")
+	//create producer
+	producer, err := initProducer()
+	if err != nil {
+		fmt.Println("Error producer: ", err.Error())
+		os.Exit(1)
+	}
+
+	headers := make(map[string]string, 0)
+	headers["header_1"] = "content_1"
+	msg := "Kafka tutorial"
+
+	produce(msg, headers, producer)
+}
+
+func initProducer() (sarama.SyncProducer, error) {
+	//setup sarama log to stdout
+	sarama.Logger = log.New(os.Stdout, "", log.Ltime)
+
+	//producer config
+	config := sarama.NewConfig()
+	config.Producer.Retry.Max = 5
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Return.Successes = true
+	config.Version = sarama.V0_11_0_0
+
+	//create producer
+	prd, err := sarama.NewSyncProducer([]string{kafkaConn}, config)
+
+	return prd, err
+}
+
+func produce(message string, headers map[string]string, producer sarama.SyncProducer) {
+	// publish sync
+	msg := &sarama.ProducerMessage{
+		Topic:   topic,
+		Value:   sarama.StringEncoder(message),
+		Headers: convertHeaders(headers),
+	}
+	p, o, err := producer.SendMessage(msg)
+	if err != nil {
+		fmt.Println("Error publish: ", err.Error())
+	}
+
+	fmt.Println("Partition: ", p)
+	fmt.Println("Offset: ", o)
+}
+
+func convertHeaders(headers map[string]string) []sarama.RecordHeader {
+	output := make([]sarama.RecordHeader, 0)
+	for key, value := range headers {
+		output = append(output, sarama.RecordHeader{
+			Key:   []byte(key),
+			Value: []byte(value),
+		})
+	}
+	return output
 }
